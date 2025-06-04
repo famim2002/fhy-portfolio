cd523f90367f26216b6046ef12cc6cab8d443e27
famim2002 <famimhayatyash@gmail.com>
2025-06-04 11:36:38 +0600

    error page setup

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Error404 = () => {
   return (
     <>
-      {/* <div className='h-dvh flex justify-center items-center '><h1 className='text-[60px] font-pixelFont text-white '>404 not found</h1></div>  */}
+      <div className='h-dvh flex justify-center items-center '><h1 className='text-[60px] font-pixelFont text-white '>404 not found</h1></div> 
     </>
   )
 }
