94a403055364fa4ad05b100c29c92757fff1955f
famim2002 <famimhayatyash@gmail.com>
2025-06-04 11:12:47 +0600

    react-router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import Layout from './layout/Index'
+import Home from './pages/Home'
+import Error404 from './pages/Error404'
 
 const App = () => {
   return (
     <div>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+          <Route index element={<Home/>}/>
+          <Route index element={<Home/>}/>
+          <Route index element={<Home/>}/>
+             
+          </Route>
+          <Route path="*" element={ <Error404/>} />
+
+          
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
---
+++ src/pages/Error404.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Error404 = () => {
+  return (
+    <>
+      {/* <div className='h-dvh flex justify-center items-center '><h1 className='text-[60px] font-pixelFont text-white '>404 not found</h1></div>  */}
+    </>
+  )
+}
+
+export default Error404
