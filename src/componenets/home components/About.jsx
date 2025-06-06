06e28cf9a8484d18e2090d2a63aa606d207c6b28
famim2002 <famimhayatyash@gmail.com>
2025-06-07 02:03:43 +0600

    about page build-up

---
+++ src/componenets/home components/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const About = () => {
+  return (
+      <section className='px-2'>
+          <div className="container">
+              <div>
+                  
+              </div>
+          </div>
+      
+    </section>
+  )
+}
+
+export default About
